5fc58156dc9acf556cbd4997d39b351ee557f578
ALi ZAffar <149040167+iam-alizaffar@users.noreply.github.com>
2024-05-18 11:23:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const inputBoxEl = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBoxEl.value === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBoxEl.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBoxEl.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function displayData() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+displayData();
